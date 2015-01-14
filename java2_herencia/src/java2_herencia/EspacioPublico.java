6eef95856d48a5abd88047e22df5f6129843940a
Sonia Ortiz de Arri <soniaodea@gmail.com>
2015-01-14 14:18:43 +0100

    empezado

---
+++ java2_herencia/src/java2_herencia/EspacioPublico.java
@@ -0,0 +1,78 @@
+package java2_herencia;
+
+public class EspacioPublico {
+
+
+		private int id;
+		private String nombre;
+		private String tipo;
+		private String direccion;
+		private String poblacion;
+		private int metros;
+		
+
+		public EspacioPublico(int i, String n, String t, String d, String p, int m)
+		{
+			id=i;
+			nombre=n;
+			tipo=t;
+			direccion=d;
+			poblacion=p;
+			metros=m;
+		}
+
+		
+		public void setId(int i)
+		{
+			this.id=i;
+		}
+		public int getId()
+		{
+			return id;
+		}
+
+		public void setNombre(String n)
+		{
+			this.nombre=n;
+		}
+		public String getNombre()
+		{
+			return nombre;
+		}
+
+		public void setTipo(String t)
+		{
+			this.tipo=t;
+		}
+		public String getTipo()
+		{
+			return tipo;
+		}
+
+		public void setDireccion(String d)
+		{
+			this.direccion=d;
+		}
+		public String getDireccion()
+		{
+			return direccion;
+		}
+
+		public void setPoblacion(String p)
+		{
+			this.poblacion=p;
+		}
+		public String getPoblacion()
+		{
+			return poblacion;
+		}
+
+		public void setMetros(int m)
+		{
+			this.metros=m;
+		}
+		public int getMetros()
+		{
+			return metros;
+		}
+}
---
+++ java2_herencia/src/java2_herencia/Parque.java
@@ -0,0 +1,12 @@
+package java2_herencia;
+
+public class Parque extends EspacioPublico {
+
+	public Parque(int i, String n, String t, String d, String p, int m) {
+		super(i, n, t, d, p, m);
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+}
